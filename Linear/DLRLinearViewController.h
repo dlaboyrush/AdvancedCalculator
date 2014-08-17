541847bfa06f727fcfeebf536da16b31c5829613
dlaboyrush <dlaboyrush@gmail.com>
2014-08-16 17:05:38 -0700

    ViewControllerFiles
    Linear view and solution views
    Two unknowns and solution views
    Quadratic and solution views

---
+++ Linear/DLRLinearViewController.h
@@ -0,0 +1,13 @@
+//
+//  DLRLinearViewController.h
+//  Advanced Calculator
+//
+//  Created by Diana  Laboy-Rush on 8/16/14.
+//  Copyright (c) 2014 Diana Laboy-Rush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLRLinearViewController : UIViewController
+
+@end
